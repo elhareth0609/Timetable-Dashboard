dd606d21405ec9bd192ad576e655822acf64b3da
elhareth0609 <elhareh0609@gmail.com>
2025-05-05 12:32:33 +0100

    ed

--- app/Interfaces/courseRepositoryInterface.php
+++ app/Interfaces/courseRepositoryInterface.php
@@ -2,7 +2,7 @@
 
 namespace App\Interfaces;
 
-interface courseRepositoryInterface {
+interface CourseRepositoryInterface {
     public function find($id);
     public function create(array $data);
     public function update($id, array $data);
